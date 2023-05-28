e3b13094e6d57419dd0cc31e443b5b9ba4df73f8
rajender <rajender2sr@gmail.com>
2023-05-28 14:34:41 +0530

    frist shell script

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,11 @@
 To print a message there are couple of commands, but echo is widely used one
 
 echo Hello world
+
+echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Magent Color\e[0m"
+echo -e "\e[36mHello in Cyan Color\e[0m"
+
