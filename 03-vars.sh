6a1c932c271f225df749ea6129724bdafab55eeb
rajender <rajender2sr@gmail.com>
2023-06-03 09:23:06 +0530

    variable basic

--- 03-vars.sh
+++ 03-vars.sh
@@ -2,7 +2,7 @@ a=10
 
 echo value of a is $adollars
 
-alternatively variables also can be accessed by using ${a}
+#alternatively variables also can be accessed by using ${a}
 
 echo value of a is ${a}adollars
 
