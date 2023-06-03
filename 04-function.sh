0c193af8356c554e504f61a96b5bf1adf6fdb0a6
rajender <rajender2sr@gmail.com>
2023-06-04 01:23:20 +0530

    variable basic

--- 04-function.sh
+++ 04-function.sh
@@ -3,3 +3,12 @@ print_hello() {
 }
 
 print_hello
+
+print_argument() {
+  echo frist argument $1
+  echo second argument $2
+  echo all arguments $*
+  echo no of arguments $#
+
+}
+print_arguments abc 123 xyz
