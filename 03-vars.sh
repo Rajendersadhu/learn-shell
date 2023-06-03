71e1ceecc1c55bfc3240268d1d00e9b1bb4a496a
rajender <rajender2sr@gmail.com>
2023-06-04 01:11:19 +0530

    variable basic

--- 03-vars.sh
+++ 03-vars.sh
@@ -8,7 +8,7 @@ echo value of a is ${a}adollars
 
 # special variables for inputs
 # special variables used for inputs are $0 to $n, $* , $#
-echo $0
-echo $1
-echo $*
-echo $#
+echo script name $0
+echo frist argument $1
+echo all arguments $*
+echo no of arguments $#
